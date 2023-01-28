c97e60980d011bfd790d81702c1d36125ab0c41a
orijoon98 <orijoon98@gmail.com>
2023-01-28 22:57:15 +0900

    Initial commit

---
+++ src/test/java/com/ssg/sausageconfigserver/SausageConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.sausageconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SausageConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
